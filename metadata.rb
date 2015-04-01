8371703a82abdec32a4a2119a0aa6d3c1be329e8
Lopaka Delp <lopaka@rightscale.com>
2015-03-31 22:55:38 -0700

    ST-1 Include build-essential cookbook and default recipe.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      'Provides a resource to manage volume backups on any cloud Righ
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.7'
 
+depends 'build-essential'
 depends 'rightscale_volume', '~> 1.2.2'
 
 recipe 'rightscale_backup::default', 'Default recipe for installing required packages/gems.'
